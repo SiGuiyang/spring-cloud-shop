3d5f6c73c51790cfcbcf315a2ec6bfc058184dd2
siguiyang <siguiyang1992@outlook.com>
2018-10-21 16:46:46 +0800

    添加商家模块

---
+++ shop-seller/src/main/java/quick/pager/shop/seller/SellerApplication.java
@@ -0,0 +1,15 @@
+package quick.pager.shop.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author siguiyang
+ */
+@SpringBootApplication
+public class SellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerApplication.class, args);
+    }
+}
---
+++ shop-seller/src/main/java/quick/pager/shop/seller/redis/RedisService.java
@@ -0,0 +1,50 @@
+package quick.pager.shop.seller.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 缓存服务
+ *
+ * @author siguiyang
+ */
+@Component
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * set
+     * @param key
+     * @param value
+     * @param time
+     */
+    public void set(String key, Serializable value, long time) {
+        redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+    }
+
+    /**
+     * get
+     * @param key
+     * @param <T>
+     * @return
+     */
+    public <T extends Serializable> T get(String key) {
+        return (T) redisTemplate.opsForValue().get(key);
+    }
+
+
+    /**
+     * del
+     * @param key
+     */
+    public void del(String key) {
+        redisTemplate.delete(key);
+    }
+    
+}
