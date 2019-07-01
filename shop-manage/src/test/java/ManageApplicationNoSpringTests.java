d8dc2d23de89ddb1353bc4ecc282bc31ecd2ebe7
siguiyang <siguiyang1992@outlook.com>
2019-07-01 14:39:56 +0800

    添加feign与hystrix配置bootstrap.yml

--- shop-manage/src/test/java/ManageApplicationNoSpringTests.java
+++ shop-manage/src/test/java/ManageApplicationNoSpringTests.java
@@ -1,11 +1,76 @@
+import java.util.Arrays;
 import org.junit.Test;
 import quick.pager.shop.model.SystemConfig;
 
 public class ManageApplicationNoSpringTests {
 
     @Test
-    public void testLong(){
+    public void testLong() {
 
-        Long lo = new Long(1L);
+        int[] arr = {2, 1, 5, 9, 4, 0, 6, 3, 8, 7};
+        insertSort(arr);
+
+        bubbleSort(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("快速排序结果：" + Arrays.toString(arr));
+
+    }
+
+    private int mid(int[] arr, int start, int end) {
+        int temp = arr[start];
+
+        while (start < end) {
+            while (start < end && arr[start] < temp)
+                start++;
+            arr[start] = arr[end];
+
+            while (start < end && arr[end] > temp)
+                end--;
+            arr[end] = arr[start];
+        }
+        arr[start] = temp;
+        return start;
+    }
+
+
+    private void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = mid(arr, start, end);
+            quickSort(arr, start, mid - 1);
+            quickSort(arr, mid + 1, end);
+
+        }
+    }
+
+
+    private void insertSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+
+            while (j > 0 && temp > arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j -= 1;
+            }
+            arr[j] = temp;
+            System.out.println(Arrays.toString(arr));
+        }
+
+    }
+
+
+    private void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("冒泡排序：" + Arrays.toString(arr));
     }
 }
